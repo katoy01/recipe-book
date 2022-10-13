d5523fddbcf274298b80cf094f013b7ec0285672
kairuihuang <6kairuih@gmail.com>
2022-10-13 00:09:51 -0400

    enhance user flow logic

--- src/App.java
+++ src/App.java
@@ -65,59 +65,75 @@ public class App {
   }
 
   // TODO: Implement
+  public static void step_through(JSONObject recipe, Scanner scanner) {
+    System.out.println("To be implemented!");
+  }
+
   // Display recipe information
-  public static void display_recipe(JSONObject recipe) {
-    System.out.printf("You have chosen %s!\n", recipe.get("name"));
+  public static void display_recipe(JSONObject recipe, Scanner scanner) {
+    while (true) {
+      System.out.printf("You have chosen %s!\n", recipe.get("name"));
+      JSONParser parser = new JSONParser();
+      try {
+        String name = (String) recipe.get("name");
+        List<String> instructions = (List<String>) recipe.get("instructions");
+        String description = (String) recipe.get("description");
+        List<String> ingredients = (List<String>) recipe.get("ingredients");
 
-    JSONParser parser = new JSONParser();
-    try {
-      String name = (String) recipe.get("name");
-      List<String> instructions = (List<String>) recipe.get("instructions");
-      String description = (String) recipe.get("description");
-      List<String> ingredients = (List<String>) recipe.get("ingredients");
-
-      System.out.printf("\tName: %s\n", name);
-      System.out.printf("\tDescription: %s\n", description);
-      System.out.printf("\tIngredients: \n");
-      for (int i = 0; i < ingredients.size(); i++) {
-        System.out.printf("\t\t(%d) %s\n", i + 1, ingredients.get(i));
+        System.out.printf("\tName: %s\n", name);
+        System.out.printf("\tDescription: %s\n", description);
+        System.out.printf("\tIngredients: \n");
+        for (int i = 0; i < ingredients.size(); i++) {
+          System.out.printf("\t\t(%d) %s\n", i + 1, ingredients.get(i));
+        }
+        System.out.printf("\tInstructions: \n");
+        for (int i = 0; i < instructions.size(); i++) {
+          System.out.printf("\t\t(%d) %s\n", i + 1, instructions.get(i));
+        }
+        // process user input
+        System.out.println("Enter (1) to go back, enter (2) to step through the instructions.");
+        String input = scanner.nextLine();
+        if (input.equals("1")) {
+          return;
+        } else if (input.equals("2")) {
+          step_through(recipe, scanner);
+        } else {
+          System.out.println("Invalid input");
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
       }
-      System.out.printf("\tInstructions: \n");
-      for (int i = 0; i < instructions.size(); i++) {
-        System.out.printf("\t\t(%d) %s\n", i + 1, instructions.get(i));
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
     }
-
   }
 
   // list all recipes and prompt user to select which one to view
   public static void list_all_recipes(Scanner scanner) {
-    System.out.println("Here are all the recipes we have stored!");
-    System.out.println("If you would like to check a specific recipe, enter the corresponding number.");
-    System.out.println("Enter `back` to go back to the main menu.");
+    while (true) {
+      System.out.println("Here are all the recipes we have stored!");
+      System.out.println("If you would like to check a specific recipe, enter the corresponding number.");
+      System.out.println("Enter `back` to go back to the main menu.");
 
-    JSONParser parser = new JSONParser();
-    JSONArray jsonarray;
-    try {
-      jsonarray = (JSONArray) parser.parse(new FileReader(RECIPEBOOK));
-      for (int i = 0; i < jsonarray.size(); i++) {
-        JSONObject recipe = (JSONObject) jsonarray.get(i);
-        String name = (String) recipe.get("name");
-        System.out.printf("\t(%d) %s\n", i + 1, name);
+      JSONParser parser = new JSONParser();
+      JSONArray jsonarray;
+      try {
+        jsonarray = (JSONArray) parser.parse(new FileReader(RECIPEBOOK));
+        for (int i = 0; i < jsonarray.size(); i++) {
+          JSONObject recipe = (JSONObject) jsonarray.get(i);
+          String name = (String) recipe.get("name");
+          System.out.printf("\t(%d) %s\n", i + 1, name);
+        }
+        // process user input
+        String input = scanner.nextLine();
+        // if user wants to go back to main menu
+        if (input.equalsIgnoreCase("back")) {
+          return;
+        }
+        // display recipe based on number
+        // TODO: handle non-integers and out of bounds integers
+        display_recipe((JSONObject) jsonarray.get(Integer.parseInt(input) - 1), scanner);
+      } catch (Exception e) {
+        e.printStackTrace();
       }
-      // process user input
-      String input = scanner.nextLine();
-      // if user wants to go back to main menu
-      if (input.equalsIgnoreCase("back")) {
-        return;
-      }
-      // display recipe based on number
-      display_recipe((JSONObject) jsonarray.get(Integer.parseInt(input) - 1)); // TODO: handle non-integers and out of
-                                                                               // bounds integers
-    } catch (Exception e) {
-      e.printStackTrace();
     }
   }
 
